ca7bc4d7f3cf7de8dc08ababb24135d819f17031
rashid <mohammed1rashid786@gmail.com>
2022-07-11 21:37:44 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/web_3_t0_do_web_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_3_t0_do_web_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:web_3_t0_do_web_app/web_three/splash/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Splash(),
+    );
+  }
+}
